e7f4756cb844d138e8de409032601d4faa42a19d
oliviermirat <miratoolivier@gmail.com>
2022-02-18 16:09:37 +0100

    fixing intro/introduction page name issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'introduction',
+            docId: 'intro',
             position: 'left',
             label: 'ZebraZoom Documentation',
           },
